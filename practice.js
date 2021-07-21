131300e2bfb9f02d65ad08e474a2477f03013a97
Alicia AyersLott <amayers92@gmail.com>
2021-07-21 13:08:44 -0500

    first commit

---
+++ practice.js
@@ -0,0 +1,71 @@
+// INTRODUCTION: AGE IN DOG YEARS
+// const calculateAgeInDogYears = (ageOfPerson) => {
+//     const ageInDogYears = ageOfPerson / 7
+    
+//     return ageInDogYears
+// }
+
+// const dogAge = calculateAgeInDogYears (45)
+// console.log(dogAge)
+
+
+
+//PRACTICE: Best In Show
+// const favDogBreed = (dogBreed) => {
+//     if (dogBreed === "meow" || "Meow") {
+//         return "I like cats";
+//     } else { 
+//         return (`My favorite dog breed is ${dogBreed}.`);
+//     }
+// }
+
+// const bestBreed = favDogBreed("Meow")
+// console.log(bestBreed)
+
+
+
+//PRACTICE: addition
+// const add = (first, second, third) => {
+//     const sum = first + second + third
+//     return sum
+// }
+
+// const total = add(17, 4, 11)
+// console.log(total)
+
+
+
+//PRACTICE: Self-Driving
+
+// const go = (direction, speed) => {
+//     if (speed > 75) {
+//         return(`The car is moving ${direction} at ${speed} mph. SLOW DOWN!`)
+//     } else {
+//         return(`The car is moving ${direction} at ${speed} mph.`)
+//     }
+// }
+
+// const carRide = go("backwards", 40)
+// console.log(carRide)
+
+
+//PRACTICE: Evens or Odds
+
+const evenOrOdd = (numberInQuestion) => {
+    if (numberInQuestion % 2 === 0) {
+        return ("even")
+    } else {
+        return ("odd")
+    }
+}
+
+let numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+for (const number of numbers {
+    const answer = evenOrOdd(number)
+    console.log(answer)
+}
+
+
+// // const answerToQuestion = evenOrOdd(44)
+// console.log(answerToQuestion)
